34f0f017e69e7adbced868bf6067018dc9013c53
abror <abror.eshkabilov@gmail.com>
2020-11-14 11:37:06 +0500

    latest

--- config/db.php
+++ config/db.php
@@ -1,12 +1,10 @@
 <?php
 
-$db_local = require __DIR__ . '/db_local.php';
-
-$default = [
+return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=sdtask',
     'username' => 'root',
-    'password' => '',
+    'password' => 'khantech1992',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
@@ -14,9 +12,3 @@ $default = [
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
 ];
-
-$default['dsn'] = $db_local['dsn'];
-$default['username'] = $db_local['username'];
-$default['password'] = $db_local['password'];
-
-return $default;
