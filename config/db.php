066bbb35a49741a86513ed6a30ba87994faace09
abror <abror.eshkabilov@gmail.com>
2020-11-14 11:39:17 +0500

    latest

--- config/db.php
+++ config/db.php
@@ -2,11 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sdtask',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=sdtask',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
-
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
